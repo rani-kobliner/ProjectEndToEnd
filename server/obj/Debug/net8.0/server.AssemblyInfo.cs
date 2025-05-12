5d0114ec565fe9ee88d5e1d87934a001b15370a8
קובלינר רני בתיה <  @mbjcomp.local>
2025-05-12 13:24:08 +0300

    first upload

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e4e3761731651a9b55cd8e3763b09542431109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5734f15008f35b21790607772ab5c10fd9cc902a")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
